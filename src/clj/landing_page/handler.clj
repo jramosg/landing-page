fcfc253aceb871078a1011c5c2022df8f4d2ebbe
j-ramos <jonurnieta@gmail.com>
2023-12-29 10:41:25 +0100

    page title

--- src/clj/landing_page/handler.clj
+++ src/clj/landing_page/handler.clj
@@ -19,7 +19,7 @@
    [:link {:href "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Roboto:wght@100&display=swap" :rel "stylesheet"}]
    [:link {:href "https://fonts.cdnfonts.com/css/fashion-fetish" :rel "stylesheet"}]
 
-   [:title "Jon"]
+   [:title "Zerbait"]
    (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
 
 (defn loading-page []
